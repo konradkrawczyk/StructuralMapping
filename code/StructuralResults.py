bc5e4fb96b16969c504d4f8fe522a89e45336574
Panzer Schnitzel <panzer.schnitzel@gmail.com>
2018-01-23 11:22:56 +0000

    Created object with most common PDB and antigen for all the elements of the set

--- code/StructuralResults.py
+++ code/StructuralResults.py
@@ -2,7 +2,9 @@
 import json,os,pprint
 from os import listdir
 from os.path import join
-from Common.Common import structural_map_location,aggregates_location
+from Common.Common import structural_map_location,aggregates_location,fetch_antigen_map
+
+
 
 #From a single map file, create an aggreagate of sequence identitities.
 def process_single_map(loc):
@@ -13,7 +15,6 @@ def process_single_map(loc):
 	#--->sequence
 	#---->#total,pdb_found,#modelable (as in certain cases because of anchors we might not have been able to model?)	
 	results = {'cdr':{},'frame':{},'full':{},'fread':{}}
-	
 
 	data = json.load(open(loc))
 
@@ -112,16 +113,131 @@ def process_experiment(exp_name):
 	f.write(json.dumps(aggregate))
 	f.close()
 
+
+#From a single map file, create an aggreagate of sequence identitities.
+def extract_data(loc,results):
+	
+	#cdr, frame and full results count the number of percentages (e.g. how many times we got sequence identity of 83%)
+	#fread holds the cdrs:
+	#h1->
+	#--->sequence
+	#---->#total,pdb_found,#modelable (as in certain cases because of anchors we might not have been able to model?)	
+	
+
+	data = json.load(open(loc))
+
+	for sequence in data:
+		
+		for region in data[sequence]:
+			if region=='fread':
+				
+				if data[sequence][region]!=None:
+					for cdr in data[sequence][region]:
+						fread_data = data[sequence][region][cdr]
+						
+						query = fread_data['qu']
+						structure = fread_data['str'][0:4]
+						if cdr not in results['cdrs']:
+							results['cdrs'][cdr] = {}
+						try: 
+							results['cdrs'][cdr][structure]['#']+=1
+							try:
+								results['cdrs'][cdr][structure]['loops'][query]+=1
+							except:
+								results['cdrs'][cdr][structure]['loops'][query] = 1
+						except KeyError:
+							results['cdrs'][cdr][structure] = {'#':1,'loops':{query:1}}
+							
+							#We do not have this pdb, need to create an entry.
+						
+			else:
+				if region == 'cdr':
+					continue
+				pdb = data[sequence][region]['best_pdb']
+				if pdb == 0:#Annotation failure.
+					continue
+				pdb = pdb[0:4]
+				try:
+					results[region][pdb]+=1
+				except KeyError:
+					results[region][pdb] = 1
+	return results
+
+#Process the results of a single experiment
+def create_summary(exp_name):
+	results_location = join(structural_map_location,exp_name)
+	i = 0
+	
+	#Fetch the aggregates.
+	antigens = fetch_antigen_map()
+
+	results = {'frame':{},'full':{},'cdrs':{}}
+
+	for f in sorted(listdir(results_location)):
+		i+=1
+		#print 'Done',i,'files...'
+		
+		results = extract_data(join(results_location,f),results)
+		if i> 100:
+			break
+		
+
+	stats = {'global':{},'frame':{},'full':{}}
+
+	for region in ['frame','full']:
+		
+		for pdb in results[region]:
+			antigen = ''
+			if pdb in antigens:
+				antigen = antigens[pdb]
+			stats[region][pdb] = {'#':results[region][pdb],'ag':antigen}
+			try:
+				stats['global'][pdb]['#']+=1
+			except KeyError:
+				stats['global'][pdb] = {'#':1,'ag':antigen} 
+	
+	#Get the most common PDBs for cdrs.
+	for cdr in results['cdrs']:
+		
+		stats[cdr] = []
+		for pdb in results['cdrs'][cdr]:
+			antigen = ''
+			if pdb in antigens:
+				antigen = antigens[pdb]
+			stats[cdr].append({'#':results['cdrs'][cdr][pdb]['#'],'pdb':pdb,'ag':antigen})
+			try:
+				stats['global'][pdb]['#']+=1
+			except KeyError:
+				stats['global'][pdb] = {'#':1,'ag':antigen} 
+			#print pdb,results['cdrs'][cdr][pdb]['#']
+		#Top 50
+		#stats[cdr] = sorted(stats[cdr],reverse=True)[0:min(len(stats[cdr]),10)]
+		#print cdr, stats[cdr]
+		#Fold results into the global aggregate.
+		#aggregate = merge_aggregates(aggregate,results)
+	import pprint
+	pprint.pprint(stats)
+	
+	#TODO top10.
+
+
+	
+
 if __name__ == '__main__':
 	
 	import sys
 	
 	cmd = sys.argv[1]
 
+	#Creating aggregates for plotting
 	if cmd == 'create_aggregate':#Usage: python StructuralResults.py create_aggregate [experiment name]
 		#Create aggregate given experiment name
 		process_experiment(sys.argv[2])
 	
+	#Create summary results.
+	if cmd == 'summary':
+		create_summary(sys.argv[2])
+
 	###########
 	#DEBUGGING#
 	###########
