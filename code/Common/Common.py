1bd8852a76719fa1650620b3e465feb325918829
Panzer Schnitzel <panzer.schnitzel@gmail.com>
2018-04-11 10:29:49 +0000

    Fixed none anarci output

--- code/Common/Common.py
+++ code/Common/Common.py
@@ -94,29 +94,28 @@ def number_sequence(query_seq):
 	#Number the query sequence
     try:
 	res = run_anarci([('q',query_seq)],scheme='chothia',assign_germline=True)
-    except:
-        return None,None
-    #Numbering failed.
-    if res[0][0] == None:
-	return None,None
-    query= res[1][0][0][0]
-    
-    chain =  res[2][0][0]['chain_type']
-    
-    #Translate the query into the format we want
-    #[chid,insert,chain] = (aa,is_CDR)
-    _query = {}
-    
-    for sid in sorted(query):
+	query= res[1][0][0][0]
+	    
+	chain =  res[2][0][0]['chain_type']
+	    
+	#Translate the query into the format we want
+	#[chid,insert,chain] = (aa,is_CDR)
+	_query = {}
+	    
+	for sid in sorted(query):
 		
 		chid = (sid[0][0],sid[0][1].replace(' ',''),chain)
 		if sid[1] == '-':
 			continue
 		_query[chid] = (sid[1],is_CDR(chain+str(sid[0][0])))
 	
-    query = _query
+	query = _query
 
-    return query,chain
+	return query,chain
+    except:
+        return None,None
+    
+	    
 	
 ###Strictly for testing purposes.	
 if __name__ == '__main__':
